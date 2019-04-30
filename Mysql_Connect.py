00e2c4143d0f6d7722f8bb8398aaca205ae693c0
NokeXie <185604383@qq.com>
2019-04-30 22:38:35 +0800

    mysql

---
+++ Mysql_Connect.py
@@ -0,0 +1,17 @@
+import pymysql
+from DBUtils.PooledDB import PooledDB
+import time
+pool = PooledDB(pymysql, 5, host='192.168.10.253', user="sa", passwd="sa", db="Car_data", port=3306, charset="utf8")
+try:
+    conn = pool.connection()
+    cur = conn.cursor()
+    sql = "select * from Car_Table "
+    cur.execute(sql)
+    results = cur.fetchall()
+    for data in results:
+        if data[3] == "解建国":
+            print("asdasdasdasd")
+
+    cur.close()
+    conn.close()
+except Exception :print("错误")
