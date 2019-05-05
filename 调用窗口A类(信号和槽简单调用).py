f963f7697921bad5f75fc37f2ee027567ef4a3df
NokeXie <185604383@qq.com>
2019-05-05 16:29:19 +0800

    信号和槽

--- 调用窗口A类(信号和槽简单调用).py
+++ 调用窗口A类(信号和槽简单调用).py
@@ -6,12 +6,17 @@ app = QApplication(sys.argv)
 win = window()
 btn = QPushButton(win)
 btn.setText("点击我")
+def cao1():
+    print("点我干嘛")
+btn.clicked.connect(cao1)
 
+def cao(nwww): #槽函数
+    print(nwww)
+win.objectNameChanged.connect(cao)
 
-def cao(): #槽函数
-    print("点我干嘛？？？？")
+win.setObjectName("sadasdasdasd111111111111111111111111111")
+print(win.objectName())
 
-
-btn.clicked.connect(cao) #点击BTN按钮触发槽函数cao()
+#btn.clicked.connect(cao) #点击BTN按钮触发槽函数cao()
 win.show()
 sys.exit(app.exec_())
