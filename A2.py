b0cdaf1a5b0527a8e63a9aa9a897bb43a636063b
NokeXie <185604383@qq.com>
2019-05-08 17:11:02 +0800

    九宫格

---
+++ A2.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.Qt import *
+from Focoserver import *
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+
+
+
+window.show()
+window.resize(500, 500)
+window.move(300, 300)
+
+# 总的控件个数
+widget_count = 10
+# 一行有多少列
+column_count = 3
+
+# 计算一个控件的宽度
+widget_width = window.width() / column_count
+# 总共有多少行 (编号 // 一行多少列 + 1)
+row_count = (widget_count - 1) // column_count + 1
+widget_height = window.height() / row_count
+
+for i in range(0, widget_count):
+    w = QLabel(window)
+    w.resize(widget_width, widget_height)
+    widget_x = i % column_count * widget_width
+    widget_y = i // column_count * widget_height
+    w.move(widget_x, widget_y)
+    w.setStyleSheet("background-color: green;border: 1px solid yellow;")
+    w.setText(foco())
+    w.show()
+
+sys.exit(app.exec_())
