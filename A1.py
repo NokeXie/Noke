04120e6b042be3456aa4342f38fe1e50c7383086
NokeXie <185604383@qq.com>
2019-05-09 09:41:04 +0800

    增加情况内容

--- A1.py
+++ A1.py
@@ -14,11 +14,19 @@ def cao():
     lable.setText(tianqi()) #tianqi()函数获取天气
     lable.adjustSize()
     lable.setStyleSheet("background-color:green;font:15px")
-    print(lable.text())
+def cao1():
+    lable.setText("") #tianqi()函数获取天气
+    lable.adjustSize()
+    lable.setStyleSheet("background-color:green;font:15px")
 btn = QPushButton(win)
 btn.resize(100,100)
 btn.move(300,100)
 btn.setText("确定")
 btn.clicked.connect(cao)
+btn1 = QPushButton(win)
+btn1.resize(100,100)
+btn1.move(300,200)
+btn1.setText("清空")
+btn1.clicked.connect(cao1)
 win.show()
 sys.exit(app.exec_())
