e022b5df0b8b127dd50e14f37b4a0b5c076a3944
NokeXie <185604383@qq.com>
2019-06-03 22:03:28 +0800

    保留foco原始版本

--- MSSQL_Connect.py
+++ MSSQL_Connect.py
@@ -1,32 +1,37 @@
 import xlwt
 import pymssql
 from 读取花名册excel数据 import renyuan
+
+
 class MSSQL:
-  def __init__(self,host,user,pwd,db):
-    self.host=host
-    self.user=user
-    self.pwd=pwd
-    self.db=db
-  def GetConnect(self):
-    if not self.db:
-      raise(NameError,'没有目标数据库')
-    self.connect=pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset='utf8')
-    cur=self.connect.cursor()
-    if not cur:
-      raise(NameError,'数据库访问失败')
-    else:
-      return cur
-  def ExecSql(self,sql):
-     cur=self.GetConnect()
-     cur.execute(sql)
-     self.connect.commit()
-     self.connect.close()
-  def ExecQuery(self,sql):
-    cur=self.GetConnect()
-    cur.execute(sql)
-    resList = cur.fetchall()
-    self.connect.close()
-    return resList
+    def __init__(self,host,user,pwd,db):
+        self.host=host
+        self.user=user
+        self.pwd=pwd
+        self.db=db
+
+    def GetConnect(self):
+        if not self.db:
+            raise(NameError,'没有目标数据库')
+        self.connect=pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset='utf8')
+        cur=self.connect.cursor()
+        if not cur:
+            raise(NameError,'数据库访问失败')
+        else:
+            return cur
+
+    def ExecSql(self,sql):
+        cur=self.GetConnect()
+        cur.execute(sql)
+        self.connect.commit()
+        self.connect.close()
+
+    def ExecQuery(self,sql):
+        cur=    self.GetConnect()
+        cur.execute(sql)
+        resList = cur.fetchall()
+        self.connect.close()
+        return resList
 
 
 def main():
@@ -46,6 +51,7 @@ def main():
             workbook.save(path)  # 保存工作簿
     print("xls格式表格写入数据成功！")
 
+
 if __name__ == '__main__':
-  main()
-  input("执行完成.................")
+    main()
+    input("执行完成.................")
