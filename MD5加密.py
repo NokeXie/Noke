c66f224d58a9d276f8ef00bef79e7c82a86bcde6
NokeXie <185604383@qq.com>
2019-06-14 16:37:45 +0800

    增加清理非在职人员功能

---
+++ MD5加密.py
@@ -0,0 +1,11 @@
+#md5进行数据的加密
+import hashlib
+#md5加密
+def encryption_md5(name):
+    m = hashlib.md5()  #创建一个hashlib.md5()对象
+    m.update(name.encode("utf8"))    #将参数转换为UTF8编码
+    print(m.hexdigest())            #用十六进制输出加密后的数据
+
+encryption_md5("lucy")
+encryption_md5("hello world")
+encryption_md5("luboyan")
